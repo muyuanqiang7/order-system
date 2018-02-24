d9457a524269f3c4bbec8b765b69908e1acb1b49
muyuanqiang <yuanqiang.mou@funi365.com>
2018-02-24 11:02:48 +0800

    api-gateway

---
+++ api-gateway/src/main/java/com/funi/muyq/order/system/gateway/ApiGatewayApplication.java
@@ -0,0 +1,21 @@
+package com.funi.muyq.order.system.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Package: [com.funi.muyq.order.system.gatewayApiGatewayApplication]
+ * @Description: []
+ * @Author: [yuanqiang.mou@funi365.com]
+ * @CreateDate: [2018/2/24上午11:00]
+ * @UpdateUser: []
+ * @UpdateDate: []
+ * @UpdateRemark: []
+ * @Version: [v1.0]
+ */
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
